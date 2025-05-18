d0ea573128897ab811169afddcee5ea00c65d1c9
rahul-gursali <145424857+rahul-gursali@users.noreply.github.com>
2025-05-18 17:52:27 +0530

    Update api-health-check.sh

--- api-health-check.sh
+++ api-health-check.sh
@@ -1,3 +1,5 @@
 # API health check
 #!/bin/bash
 curl -s -o /dev/null -w "%{http_code}" http://api.example.com/health
+
+# if the service is healthy, it might return a 200, but if the service is down, it might return a 500 or 404.
